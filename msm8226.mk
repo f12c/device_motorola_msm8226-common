610099a5de1be20fa334047e9a9109a8b7f0c745
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:11:27 +0200

    msm8226-common: Build Camera HAL

--- msm8226.mk
+++ msm8226.mk
@@ -91,9 +91,11 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
+    camera.msm8226 \
     libxml2 \
     libshims_camera \
-    camera.msm8226 \
     Snap
 
 # DRM
